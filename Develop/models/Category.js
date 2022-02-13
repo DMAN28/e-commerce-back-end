0af965b9db4ed94cea5ba1c00dcc1f3d271bfe5c
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-02-13 09:50:35 -0800

    category routes

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -5,7 +5,18 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
+  { 
+    id :{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  category_name:{
+    type: DataTypes.string,
+    allowNull: false,
+  },
+
     //TODO Category model is created with two properties: id and category name.
   },
   {
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,17 +4,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // find all tags findAll method on Tag's model object to return all tags
+  // be sure to include its associated Product data include data from products as well as tag_name attributes.
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+  // find a single tag by its `id` looking for a single tag by its id and then it will fetch the associated product data.
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // create a new tag in the database and returns to client
 });
 
 router.put('/:id', (req, res) => {
