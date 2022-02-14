d4d28a1d074572f6f61b54f141476ecb003a9795
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-02-13 21:44:05 -0800

    Syntax correction

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const categoryRoute = require('../routes/api/category-routes')
-const productRoute = require('../routes/api/product-routes')
-const tagRoute = require('../routes/api/tag-routes')
-//const apiRoutes = require('./api');
+// const categoryRoute = require('../routes/api/category-routes')
+// const productRoute = require('../routes/api/product-routes')
+// const tagRoute = require('../routes/api/tag-routes')
+const apiRoutes = require('./api');
 
-//router.use('/api', apiRoutes);
-router.use('/categories',categoryRoute);
-router.use('/products', productRoute);
-router.use('/tags',tagRoute)
+router.use('/api', apiRoutes);
+// router.use('/categories',categoryRoute);
+// router.use('/product', productRoute);
+// router.use('/tag',tagRoute)
 
-// router.use((req, res) => {
-//   res.send("<h1>Wrong Route!</h1>")
-// });
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
 
 module.exports = router;
